484d5426a52de8d80d03d3bdc9e8e21e0cf96419
vamandeshmukh <vamandeshmukh@gmail.com>
2021-04-07 16:28:27 +0530

    Commit

--- src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
+++ src/main/java/com/cg/spring/boot/demo/ComCgSpringBootDemoApplication.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// test comment 
+
+
 @SpringBootApplication
 
 public class ComCgSpringBootDemoApplication {
--- src/test/java/com/cg/spring/boot/demo/EmployeeServiceTests.java
+++ src/test/java/com/cg/spring/boot/demo/EmployeeServiceTests.java
@@ -3,6 +3,7 @@ package com.cg.spring.boot.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.springframework.beans.factory.annotation.Autowired;
+// this is a comment 
 
 import com.cg.spring.boot.demo.model.Employee;
 import com.cg.spring.boot.demo.service.EmployeeService;
